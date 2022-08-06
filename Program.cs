1ed4d0f36313e38033b35ea8a73b86ec037c75f9
Albina <albina.half@gmail.com>
2022-08-06 18:08:43 +0500

    Task 25 has been solved

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+
+Console.Write("Enter a number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine($"Result = {result}");
